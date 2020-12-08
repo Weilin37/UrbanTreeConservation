6ff5d2e307123878899654a5c1a919de62c160d3
Weilin Meng <wm84@cornell.edu>
2020-12-07 21:22:56 -0500

    added search bar

--- client/src/js/components/NavBar.js
+++ client/src/js/components/NavBar.js
@@ -1,20 +1,24 @@
 import React, { useState, useEffect } from "react";
-import { connect } from "react-redux";
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import SearchBar from "./SearchBar";
+import Box from '@material-ui/core/Box';
 
 const NavBar = props => {
-
     return (
       <AppBar position="static">
           <Toolbar variant="dense">
+            <IconButton edge="start" color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
             <Typography variant="h6" color="inherit">
               Urban Tree Conservation
             </Typography>
+            <Box display="flex" justify="flex-end"><SearchBar /></Box>
           </Toolbar>
       </AppBar>
     );
--- server/main/routes.js
+++ server/main/routes.js
@@ -22,4 +22,12 @@ router.get('/api/get/markers', (req,res,next) => {
 		})
 })
 
+// Get search latlng
+router.get('/api/get/search', (req,res,next) => {
+	pool.query(`select * from public.standard_dataset where latitude is not null and longitude is not null limit 100`,
+		(q_err, q_res) => {
+			res.json(q_res.rows)
+		})
+})
+
 module.exports = router
