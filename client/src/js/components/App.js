dde5111f8a754760982e4b46fd9bcf4410bdcd6a
Weilin Meng <wm84@cornell.edu>
2020-12-08 23:55:57 -0500

    adding analytic features

--- client/src/js/components/App.js
+++ client/src/js/components/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import NavBar from "./NavBar";
 import { LeafMap } from "./Map";
 import Grid from '@material-ui/core/Grid';
+import { VictoryBar } from 'victory';
+import Paper from '@material-ui/core/Paper';
 
 const App = () => (
-  <Grid container justify="center" alignItems="center" spacing={2} >
+  <Grid container justify="center" alignItems="center" spacing={2}>
     <NavBar />
-    <Grid item align="center" xs={12} >
-        <LeafMap />
-    </Grid>
+    { false ? <Grid item align="center" xs={6} ><Paper><VictoryBar /></Paper></Grid> : null }
+    { false ? <Grid item align="center" xs={6} ><LeafMap /></Grid> : <Grid item align="center" xs={12} ><LeafMap /></Grid> }
   </Grid>
 );
 
